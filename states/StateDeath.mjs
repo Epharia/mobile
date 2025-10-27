c409f246957447fea03adb3830e2960e76c2378f
Epharia <emelybarth@yahoo.com>
2025-10-27 20:00:48 +0100

    respawn after time

--- states/StateDeath.mjs
+++ states/StateDeath.mjs
@@ -3,9 +3,11 @@ import { World } from "../world/world.mjs";
 import { State } from "./State.mjs";
 
 export class StateDeath {
-
+    static timer = 5;
     tick() {
-        if (Handler.input.keys.pause.pressed) {
+        StateDeath.timer -= Handler.delta;
+        if (Handler.input.keys.pause.pressed || StateDeath.timer <= 0) {
+            StateDeath.timer = 5;
             Handler.world = new World();
             Handler.world.init();
             State.setState(State.game);
@@ -19,7 +21,8 @@ export class StateDeath {
         ctx.textAlign = "center";
         ctx.shadowColor = 'rgba(255, 0, 0, 0.6)';
         ctx.shadowBlur = 16;
-        ctx.fillText("You Died!", canvas.width / 2, canvas.height / 2);
+        ctx.fillText('You Died!', canvas.width / 2, canvas.height / 2 - 100);
+        ctx.fillText(`Respawn in ${Math.ceil(StateDeath.timer)}`, canvas.width / 2, canvas.height / 2 + 200);
         ctx.restore();
     }
 }
