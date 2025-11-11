6cf2f03a2cb2eae1e33f4575ea36082995f4473d
Epharia <emelybarth@yahoo.com>
2025-11-11 15:47:14 +0100

    refactor AiFollow class to include range parameter and improve enemy convergence logic

--- entities/ai/tasks/aiFollow.mjs
+++ entities/ai/tasks/aiFollow.mjs
@@ -7,8 +7,10 @@ export class AiFollow extends AIBase {
     /**
      * @param {Sprite} sprite 
      */
-    constructor(sprite) {
+    constructor(sprite, range) {
         super();
+        this.range2 = range ? range * range : null;
+        if (this.range2) console.log(this.range2)
         this.sprite = sprite;
         this.flags = 1;
     }
@@ -16,21 +18,32 @@ export class AiFollow extends AIBase {
     tick() {
         const direction = this.#playerVector.normalize();
 
-        //Prevent Converging
-        const modifier = Handler.world.densityModifier;
-        if (modifier > 0) {
-            let enemies = Handler.world.entities.list.filter((e) => e instanceof Enemy);
-            if (enemies.length == 0) return;
-            for (let e of enemies) {
-                const offset = this.sprite.pos.copy.sub(e.pos);
-                const distance2 = offset.magnitude2;
-                if (distance2 == 0) continue;
-                const strength = (5000 / (distance2)) / modifier;
-                direction.addScaled(offset.normalize(), strength);
+        if (this.sprite instanceof Enemy) {
+            //Prevent Converging
+            const modifier = Handler.world.densityModifier;
+            if (modifier > 0) {
+                let enemies = Handler.world.entities.list.filter((e) => e instanceof Enemy);
+                if (enemies.length === 0) return;
+                for (let e of enemies) {
+                    const offset = this.sprite.pos.copy.sub(e.pos);
+                    const distance2 = offset.magnitude2;
+                    if (distance2 === 0) continue;
+                    const strength = (5000 / (distance2)) / modifier;
+                    direction.addScaled(offset.normalize(), strength);
+                }
             }
         }
 
         this.sprite.accelerate(direction);
+    }
+
+    onStart() {
+        this.tick();
+    }
+
+    get shouldExecute() {
+        if (this.range2 === null) return true;
+        return this.#playerVector.magnitude2 < this.range2;
     }
 
     //HELPER
